145e70ede2deef28a1489102e3a24eb69873534a
Josh <graham.josh@gmail.com>
2013-08-03 15:19:07 -0500

    add ticket detail page

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,4 +4,5 @@ from tickets import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<ticket_id>\d+)/$', views.ticket, name='ticket')
 )
--- tickets/views.py
+++ tickets/views.py
@@ -10,3 +10,13 @@ def index(request):
         'tickets/index.html',
         {'tickets': tickets}
     )
+
+
+def ticket(request, ticket_id):
+    ticket = Ticket.objects.get(pk=ticket_id)
+
+    return render(
+        request,
+        'tickets/ticket.html',
+        {'ticket': ticket}
+    )
