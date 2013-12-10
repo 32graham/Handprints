edfa648c675dcbb3a774147a50c085fd7b8e1820
Josh <graham.josh@gmail.com>
2013-12-09 20:48:32 -0600

    finish removing pagination app

--- handprints/settings/base.py
+++ handprints/settings/base.py
@@ -77,7 +77,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'pagination.middleware.PaginationMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
