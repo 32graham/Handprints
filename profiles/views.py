b34cd12e05672483b6cb41c93ca615bf7340894e
Josh <graham.josh@gmail.com>
2014-01-05 14:54:51 -0600

    protect profile page with login

--- profiles/views.py
+++ profiles/views.py
@@ -2,12 +2,18 @@ from .models import Profile
 from django.views.generic import DetailView
 from django.core.urlresolvers import reverse
 from django.contrib.auth.views import password_reset, password_reset_confirm
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 class ProfileDetailView(DetailView):
     model = Profile
     context_object_name = 'profile'
     template_name = 'profiles/user_detail.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProfileDetailView, self).dispatch(*args, **kwargs)
 
 
 def reset_confirm(request, uidb36=None, token=None):
