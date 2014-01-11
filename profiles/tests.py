8e599c8160b171eb343350bb8003f262c5e69000
Josh <graham.josh@gmail.com>
2014-01-11 14:31:43 -0600

    fix profile test

--- profiles/tests.py
+++ profiles/tests.py
@@ -9,11 +9,12 @@ class TicketsViewsTestCase(TestCase):
 
     def setUp(self):
         user = User.objects.create_user(username='username', password='password')
+        user.is_staff = True
+        user.save()
         company = Company.objects.create(name='company')
         Profile.objects.create(company=company, user=user)
 
-
     def test_profile(self):
         self.client.login(username='username', password='password')
-        resp = self.client.get(reverse('profile', args=[1]))
+        resp = self.client.get(reverse('profile', args=(1,)))
         self.assertEqual(resp.status_code, 200)
