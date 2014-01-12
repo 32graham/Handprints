b2af367606e8c2c08fe203823687986923e5140b
Josh <graham.josh@gmail.com>
2014-01-12 15:13:09 -0600

    secure media with login

--- handprints/urls.py
+++ handprints/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from haystack.views import basic_search
 from tickets.views import IndexView
 from django.conf import settings
+from django.views.static import serve
 
 
 admin.autodiscover()
@@ -20,7 +21,7 @@ urlpatterns = patterns(
     url(r'^select2/', include('django_select2.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='auth_login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='auth_logout'),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', login_required(serve), {'document_root': settings.MEDIA_ROOT}),
     url(r'^reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'profiles.views.reset_confirm', name='password_reset_confirm'),
     url(r'^reset/$', 'profiles.views.reset', name='password_reset'),
 )
