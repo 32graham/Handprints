7d3ffb93e7135ec61fb76d9c34deef0c9ab99d15
Josh <graham.josh@gmail.com>
2014-01-24 22:09:34 -0600

    further lock down profile settings page; allow profiles/me/ url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from .views import ProfileDetailView, ProfileSettingsView
+from .views import ProfileDetailView, ProfileSettingsView, me
 
 urlpatterns = patterns(
     '',
     url(r'^(?P<pk>\d+)/$', ProfileDetailView.as_view(), name='profile'),
-    url(r'^(?P<pk>\d+)/settings/$', ProfileSettingsView.as_view(), name='profile_settings'),
+    url(r'^me/$', me, name='profile_me'),
+    url(r'^me/settings/$', ProfileSettingsView.as_view(), name='profile_settings'),
 )
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
 from .models import Profile
+from django.shortcuts import render
 from django.views.generic import DetailView
 from django.core.urlresolvers import reverse
 from django.views.generic.edit import UpdateView
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import password_reset, password_reset_confirm
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
@@ -21,6 +23,25 @@ class ProfileSettingsView(UpdateView):
     model = Profile
     fields = ['theme']
     template_name = 'profiles/profile_settings.html'
+
+    def get_object(self):
+        return self.request.user.profile
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProfileSettingsView, self).dispatch(*args, **kwargs)
+
+
+@login_required
+def me(request):
+    profile = Profile.objects.get(pk=request.user.profile.pk)
+
+    return render(
+        request,
+        'profiles/user_detail.html',
+        {
+            'profile': profile
+        })
 
 
 def reset_confirm(request, uidb36=None, token=None):
